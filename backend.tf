136b7060e1214eaadaa8cb44d67621f2b6a9a5e7
narendraba <ballipallinarendra@gmail.com>
2024-12-25 16:43:22 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "spliting-bucket2"
-    key            = "workspaces/${terraform.workspace}/terraform.tfstate"
+    key            = "workspaces/playback/terraform.tfstate"
     region         = "ap-south-1"
     encrypt        = true
     dynamodb_table = "networking"
