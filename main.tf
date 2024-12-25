a2d8ec5f8eef719d8472e7409c8f8dd27ed10dac
narendraba <ballipallinarendra@gmail.com>
2024-12-25 16:39:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
-terraform {
-  backend "s3" {
-    bucket         = "spliting-bucket2"
-    key            = "workspaces/${terraform.workspace}/terraform.tfstate"
-    region         = "ap-south-1"
-    encrypt        = true
-    dynamodb_table = "networking"
-  }
-}
+#terraform {
+#  backend "s3" {
+#    bucket         = "spliting-bucket2"
+#    key            = "workspaces/${terraform.workspace}/terraform.tfstate"
+#    region         = "ap-south-1"
+#    encrypt        = true
+#    dynamodb_table = "networking"
+#  }
+#}
 
 provider "aws" {
   region = var.aws_region
