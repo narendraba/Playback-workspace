2fb3c1515d89e9555d7f93a4799abfcfd42a659e
narendraba <ballipallinarendra@gmail.com>
2024-12-25 15:58:29 +0530

    Create main.tf

---
+++ terraform-workspace/modules/ec2/main.tf
@@ -0,0 +1,13 @@
+resource "aws_instance" "compute" {
+  #ami           = "ami-053b12d3152c0cc71"
+  ami           = var.ami_id
+  #instance_type = "t2.micro"
+  instance_type = var.instance_type
+  tags = {
+    Name = "compute-instance"
+  }
+output "instance_id" {
+  value = aws_instance.main.id
+  }
+  
+}
