a6cae1e0baaebd3f55fe093ed30e246f63e91120
narendraba <ballipallinarendra@gmail.com>
2024-12-25 16:00:54 +0530

    Update main.tf

--- terraform-workspace/modules/vpc/main.tf
+++ terraform-workspace/modules/vpc/main.tf
@@ -1,13 +1,6 @@
-resource "aws_instance" "compute" {
-  #ami           = "ami-053b12d3152c0cc71"
-  ami           = var.ami_id
-  #instance_type = "t2.micro"
-  instance_type = var.instance_type
-  tags = {
-    Name = "compute-instance"
-  }
-output "instance_id" {
-  value = aws_instance.main.id
+resource "aws_vpc" "main" {
+  cidr_block = var.cidr_block
 }
-  
+output "vpc_id" {
+  value = aws_vpc.main.id
 }
