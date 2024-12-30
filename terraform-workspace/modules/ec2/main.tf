1d642fc2454322fe2eb0de5e3fe7145d74b1aec7
narendraba <ballipallinarendra@gmail.com>
2024-12-30 11:12:22 +0530

    Update main.tf

--- terraform-workspace/modules/ec2/main.tf
+++ terraform-workspace/modules/ec2/main.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "compute" {
   #ami           = "ami-053b12d3152c0cc71"
-  ami           = var.ami_id
+  ami           = var.ec2_ami_id
   #instance_type = "t2.micro"
-  instance_type = var.instance_type
+  instance_type = var.ec2_instance_type
   tags = {
     Name = "compute-instance"
   }
